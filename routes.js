c771a57723c229c3531b410b8cea7c37d63626f2
Luis Pizarro <pizarro_16_34@hotmail.com>
2021-04-14 09:44:03 -0500

    Prueba Subida

---
+++ routes.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT * FROM book', (err, rows)=> {
+            if(err) return res.send(err)
+    
+            res.json(rows)
+        })
+    })  
+})
+
+routes.post('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('INSERT INTO book set ?', [req.body], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('book added!')
+        })
+    })
+})
+
+routes.delete('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('DELETE FROM book WHERE id = ?', [req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('book excluded!')
+        })
+    })
+})
+
+routes.put('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('UPDATE book set ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('book updated!')
+        })
+    })
+})
+
+module.exports = routes
